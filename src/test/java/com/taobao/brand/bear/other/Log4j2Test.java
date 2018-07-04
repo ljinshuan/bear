56e0536ff791040dab800fd8e5f45e676b7cd64a
jinshuan.li <jinshuan.li@alibaba-inc.com>
2018-07-04 15:56:16 +0800

    add

--- src/test/java/com/taobao/brand/bear/other/Log4j2Test.java
+++ src/test/java/com/taobao/brand/bear/other/Log4j2Test.java
@@ -1,15 +1,15 @@
-package com.taobao.brand;
+package com.taobao.brand.bear.other;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * @author jinshuan.li 2018/7/4 10:49
  */
 public class Log4j2Test {
 
-    private static final Logger logger = LoggerFactory.getLogger(Log4j2Test.class.getName());
+    private static final Logger logger = LogManager.getLogger(Log4j2Test.class.getName());
 
     @Test
     public void test() throws InterruptedException {
