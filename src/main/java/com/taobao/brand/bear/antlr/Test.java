ae76d54ac536100a3a9400f0cf464e368c499764
jinshuan.li <jinshuan.li@alibaba-inc.com>
2018-07-06 21:48:17 +0800

    数据转换 convert

--- src/main/java/com/taobao/brand/bear/antlr/Test.java
+++ src/main/java/com/taobao/brand/bear/antlr/Test.java
@@ -1,8 +1,9 @@
 package com.taobao.brand.bear.antlr;
 
-
-import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CodePointCharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 /**
  * @author jinshuan.li 2018/6/24 15:12
@@ -11,13 +12,18 @@ public class Test {
 
     public static void main(String[] args) {
 
-        ANTLRStringStream input = new ANTLRStringStream("{99,{2},3445}");
-        ArrayInitLexer lexter = new ArrayInitLexer((CharStream)input);
+        CodePointCharStream codePointCharStream = CharStreams.fromString("{99,33,3445}");
+        ArrayInitLexer lexter = new ArrayInitLexer(codePointCharStream);
         CommonTokenStream tokens = new CommonTokenStream(lexter);
         ArrayInitParser parser = new ArrayInitParser(tokens);
 
         ArrayInitParser.InitContext init = parser.init();
 
         System.out.println(init.toStringTree(parser));
+
+        ParseTreeWalker walker = new ParseTreeWalker();
+        walker.walk(new ShortToUnicodeString(), init);
+
+        System.out.println();
     }
 }
